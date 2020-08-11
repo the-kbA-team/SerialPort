771522ab1dafd4e479675a0fcee3a3599e35bb06
Gregor <gregor-j@users.noreply.github.com>
2020-08-11 12:49:35 +0200

    remove FIFO tests

--- tests/SerialPortTest.php
+++ tests/SerialPortTest.php
@@ -23,25 +23,6 @@ class SerialPortTest extends TestCase
     public const ECHO_PORT = 9999;
 
     /**
-     * TCP port ncat listens on.
-     */
-    public const FIFO_PORT = 9998;
-
-    /**
-     * @var string
-     */
-    private $fifo;
-
-    /**
-     * @inheritDoc
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->fifo = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'fifoService';
-    }
-
-    /**
      * Test connection retries.
      */
     public function testConnectionFailed(): void
@@ -224,39 +205,4 @@ class SerialPortTest extends TestCase
         $this->expectExceptionMessage('Timeout below 0.');
         $device->setTimeout(-1.0);
     }
-
-//    /**
-//     * Test the EOF exception while waiting for a termination character, that
-//     * doesn't appear.
-//     */
-//    public function testReadUntilEofException(): void
-//    {
-//        file_put_contents($this->fifo, 'B', FILE_APPEND);
-//        $serial = new SerialPort(new Socket('127.0.0.1', self::FIFO_PORT), 1, 0);
-//        $serial->setTimeout(0.5);
-//        $exception = null;
-//        try {
-//            $serial->readUntil('A');
-//        } catch (EofException $exception) {
-//        }
-//        static::assertInstanceOf(EofException::class, $exception, 'EofException was never thrown!');
-//        static::assertSame('B', $exception->getResponse());
-//    }
-//
-//    /**
-//     * Test the EOF exception while waiting for a termination character, that
-//     * doesn't appear.
-//     */
-//    public function testReadUntilTimeoutException(): void
-//    {
-//        $serial = new SerialPort(new Socket('127.0.0.1', self::ECHO_PORT), 1, 0);
-//        $serial->setTimeout(0.5);
-//        $exception = null;
-//        try {
-//            $serial->readUntil('A');
-//        } catch (TimeoutException $exception) {
-//        }
-//        static::assertInstanceOf(TimeoutException::class, $exception, 'TimeoutException was never thrown!');
-//        static::assertSame('', $exception->getResponse());
-//    }
 }
